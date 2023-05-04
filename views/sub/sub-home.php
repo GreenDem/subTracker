4b5063c8f0743266e09f444c850bd0da57127b8f
Nathan <demory.nathan@gmail.com>
2023-05-04 16:52:21 +0200

    HomepageLogged

---
+++ views/sub/sub-home.php
@@ -0,0 +1,51 @@
+<div class="top">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus illo nobis atque mollitia optio repellendus nemo eos distinctio quidem, placeat natus? Obcaecati eligendi in vero recusandae fugiat deleniti vitae non!</p>
+</div>
+<div class="cardContainer">
+    <div class="card">
+        <div class="cardImg">
+            <img src="/public/assets/img/deezerLogo.jpg" alt="">
+        </div>
+        <div class="cardText">
+        <div class="leftCardText"></div>
+        <div class="rightCardText"></div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardImg">
+            <img src="/public/assets/img/deezerLogo.jpg" alt="">
+        </div>
+        <div class="cardText">
+        <div class="leftCardText"></div>
+        <div class="rightCardText"></div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardImg">
+            <img src="/public/assets/img/deezerLogo.jpg" alt="">
+        </div>
+        <div class="cardText">
+        <div class="leftCardText"></div>
+        <div class="rightCardText"></div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardImg">
+            <img src="/public/assets/img/deezerLogo.jpg" alt="">
+        </div>
+        <div class="cardText">
+        <div class="leftCardText"></div>
+        <div class="rightCardText"></div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardImg">
+            <img src="/public/assets/img/deezerLogo.jpg" alt="">
+        </div>
+        <div class="cardText">
+        <div class="leftCardText"></div>
+        <div class="rightCardText"></div>
+        </div>
+    </div>
+
+</div>
